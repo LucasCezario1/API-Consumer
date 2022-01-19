43bfe5e265b236f54ab11867f3c038c82e7b7f33
Lucas Cezario <lucasdc96@gmail.com>
2022-01-19 11:11:52 -0300

    Adicionado o java:11 no proprieties

--- src/main/java/com/webcode/Webclientapiconsumer/client/RickandMortyClient.java
+++ src/main/java/com/webcode/Webclientapiconsumer/client/RickandMortyClient.java
@@ -8,13 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 @Service
 @Slf4j
 public class RickandMortyClient {
 
-    private final WebClient webCLient;
+
+    WebClient webCLient;
 
     public RickandMortyClient(WebClient.Builder builder) {
         webCLient = WebClient.builder().baseUrl("https://rickandmortyapi.com/api").build();
@@ -26,7 +25,7 @@ public class RickandMortyClient {
         return webCLient
                 .get()
                 .uri("/character/"+id)
-                .accept(APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, error -> Mono.error(new RuntimeException("Vereficar os paramentos")))
                 .bodyToMono(CharacterResponse.class);
